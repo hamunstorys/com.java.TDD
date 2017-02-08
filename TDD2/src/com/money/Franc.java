59a960106c4177e83b0c0eb8c13e8021e7841469
it <it@DESKTOP-KKAQ0RK>
2017-02-08 09:19:38 +0900

    Revision 9 part 10

--- TDD2/src/com/money/Franc.java
+++ TDD2/src/com/money/Franc.java
@@ -6,11 +6,6 @@ public class Franc extends Money {
 		super(amount, currency);
 	}
 
-	public Money times(int multiplier) {
-		return Money.franc(amount * multiplier);
-
-	}
-
 	public String currency() {
 		return currency;
 
