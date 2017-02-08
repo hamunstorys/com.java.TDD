705edbb57b51be71e48cd5a32bd0ac6ae65747bb
it <it@DESKTOP-KKAQ0RK>
2017-02-08 09:23:18 +0900

    Revision 10 TDD2 part 11

--- TDD2/src/com/test/MoneyTest.java
+++ TDD2/src/com/test/MoneyTest.java
@@ -31,9 +31,7 @@ public class MoneyTest extends TestCase {
 	@Test
 	public void testEqualiy() {
 		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
-		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-		assertTrue(Money.franc(5).equals(Money.franc(5)));
-		assertFalse(Money.franc(5).equals(Money.franc(6)));
+		assertFalse(Money.dollar(5).equals(Money.dollar(6)));;
 		assertFalse(Money.dollar(5).equals(Money.franc(5)));
 
 	}
